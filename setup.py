b20eee0c1fb7ab791b05571b026ff29228c9ebf8
Justin Poliey <justin.d.poliey@gmail.com>
2012-04-29 21:24:17 -0400

    dry up the package metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
-from taxon import __version__
-
+from taxon import __version__, __author_name__, __author_email__
 
 with open('README.rst') as f:
     readme = f.read()
@@ -15,8 +13,8 @@ setupinfo = dict(
     version=__version__,
     description='Redis-backed tagged data store',
     long_description=readme,
-    author='Justin Poliey',
-    author_email='justin@getglue.com',
+    author=__author_name__,
+    author_email=__author_email__,
     url='https://github.com/jdp/taxon',
     keywords=['redis', 'key-value', 'store', 'tag', 'taxonomy'],
     packages=find_packages(exclude=('tests', 'docs')),
--- taxon/__init__.py
+++ taxon/__init__.py
@@ -1,6 +1,8 @@
 from .core import Taxon
 
-__author__ = "Justin Poliey <justin@getglue.com>"
+__author_name__ = "Justin Poliey"
+__author_email__ = "justin@getglue.com"
+__author__ = "%s <%s>" % (__author_name__, __author_email__)
 __license__ = "MIT"
 __version__ = '0.2.0'
 
