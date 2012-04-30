9e3a0c6d2235cce42eee70ce5b44cf8e160eecd3
Justin Poliey <justin.d.poliey@gmail.com>
2012-04-29 22:02:22 -0400

    add some basic docs to the query dsl

--- taxon/query.py
+++ taxon/query.py
@@ -12,6 +12,15 @@ def sexpr(val):
 
 
 class Query(object):
+    """
+    Taxon is queried by ``dict`` objects that represent the syntax tree of the
+    query. These dict queries can become unwieldy quickly, so the query DSL is
+    provided as the main way to query Taxon.
+
+    All subclasses of ``Query`` implement a ``freeze`` method which builds the
+    dict representation of the query.
+    """
+
     def __init__(self, expr):
         self.expr = expr
         self.children = []
@@ -27,6 +36,7 @@ class Query(object):
 
     @classmethod
     def coerce(self, expr):
+        "Returns the ``Query`` representation of the expression."
         if isinstance(expr, Query):
             return expr
         elif isinstance(expr, basestring):
@@ -35,15 +45,19 @@ class Query(object):
             raise TypeError("Expected %s or string, got %s" % (Query.__name__, expr))
 
     def freeze(self):
+        "Returns the ``dict`` representation of the query expression."
         return {self.op: [c.freeze() for c in self.children]}
 
 
 class Tag(Query):
+    "Returns the items with the specified tag."
     def freeze(self):
         return {"tag": [self.expr]}
 
 
 class And(Query):
+    "Returns the items matched by all ``Query`` expressions."
+
     op = "and"
 
     def __init__(self, *exprs):
@@ -51,6 +65,8 @@ class And(Query):
 
 
 class Or(Query):
+    "Returns the items matched by any or all of the ``Query`` expressions."
+
     op = "or"
 
     def __init__(self, *exprs):
@@ -58,6 +74,8 @@ class Or(Query):
 
 
 class Not(Query):
+    "Returns the items **not** matched by any of the ``Query`` expressions."
+    
     def __init__(self, expr):
         self.expr = Query.coerce(expr)
 
