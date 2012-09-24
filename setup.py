7e186942d28f85e6d94f9ab1a18ba8c74cc69d95
Justin Poliey <justin@getglue.com>
2012-09-24 00:06:56 -0400

    version 0.3.0 release

--- setup.py
+++ setup.py
@@ -21,7 +21,26 @@ setupinfo = dict(
     include_package_data=True,
     packages=find_packages(exclude=('tests', 'docs')),
     license=license,
-    requires=['redis']
+    requires=['redis'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Systems Administration',
+    ]
 )
 
 try:
--- taxon/__init__.py
+++ taxon/__init__.py
@@ -4,6 +4,6 @@ __author_name__ = "Justin Poliey"
 __author_email__ = "justin@getglue.com"
 __author__ = "%s <%s>" % (__author_name__, __author_email__)
 __license__ = "MIT"
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 
 VERSION = tuple(map(int, __version__.split('.')))
